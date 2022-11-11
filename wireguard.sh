5e3d91b6fd2012c7b8ed0bb5055b374750bac797
Javier Torres <javier@javiertorres.eu>
2022-11-11 22:50:50 +0100

    Support multiple allowed-ips when there are spaces

--- wireguard.sh
+++ wireguard.sh
@@ -12,7 +12,7 @@ trap "rm -f /var/run/wireguard/$WG_TUN.sock; sleep 0.5; exit" SIGTERM SIGINT
 /sbin/ip addr add $WG_INTERFACE_ADDRESS dev $WG_TUN
 KEYFILE=$(mktemp)
 echo $WG_INTERFACE_PRIVATE_KEY > $KEYFILE
-wg set $WG_TUN private-key $KEYFILE peer $WG_PEER_PUBLIC_KEY endpoint $WG_PEER_ENDPOINT allowed-ips $WG_PEER_ALLOWED_IPS persistent-keepalive $WG_PEER_PERSISTENT_KEEPALIVE
+wg set $WG_TUN private-key $KEYFILE peer $WG_PEER_PUBLIC_KEY endpoint $WG_PEER_ENDPOINT allowed-ips "$WG_PEER_ALLOWED_IPS" persistent-keepalive $WG_PEER_PERSISTENT_KEEPALIVE
 rm $KEYFILE
 
 dbus-send --system --dest=$CONNMAN_BUSNAME --type=method_call $CONNMAN_PATH $CONNMAN_INTERFACE.notify string:up
